9f370a0e36e529da12a710d72607279f2db8d47d
Pritam Lendale <7822058623+PL7822@users.noreply.github.com>
2026-02-18 19:15:42 +0530

    Fixed Netlify MIME issue

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/",   // 🔥 IMPORTANT LINE ADD KAR
+  base: "/",   
   server: {
     host: "::",
     port: 8080,
