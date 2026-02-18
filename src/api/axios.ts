034e1d37248b39ba4dfab4e69ee2bafb0a443b14
Pritam Lendale <7822058623+PL7822@users.noreply.github.com>
2026-02-19 02:24:10 +0530

    Fixed Netlify MIME issue

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "hhttps://careerpath-india.onrender.comom/api"
+  baseURL: "hhttps://careerpath-india.onrender.com/api"
 });
 
 // 🔐 Attach token automatically
