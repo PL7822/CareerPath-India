570e823222f99188e920d50330cb7d1ddabaf946
Pritam Lendale <7822058623+PL7822@users.noreply.github.com>
2026-02-19 03:34:55 +0530

    Add CORS

--- server/server.js
+++ server/server.js
@@ -150,9 +150,11 @@ app.post("/api/auth/login", async (req, res) => {
       user: {
         _id: user._id,
         name: user.name,
-        email: user.email
+        email: user.email,
+        createdAt: user.createdAt
       }
     });
+
 
   } catch (err) {
     console.log("Login Error:", err);
