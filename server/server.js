f0702a365be910652e033ec15a02169b88685f54
Pritam Lendale <7822058623+PL7822@users.noreply.github.com>
2026-02-19 13:53:53 +0530

    app.Listen change

--- server/server.js
+++ server/server.js
@@ -197,7 +197,8 @@ app.get("/api/auth/profile", async (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log("Server running on port " + PORT + " 🚀");
 });
+
 
